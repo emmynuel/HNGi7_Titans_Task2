67846b15061e8c2bf144cff883a76d76d5627099
emmynuel <emmynuel02@gmail.com>
2020-06-07 12:53:54 +0100

    Create MoonEast_.php

---
+++ scripts/MoonEast_.php
@@ -0,0 +1,10 @@
+<?php
+
+$NAME = "Bamidele Emmanuel";
+$ID = "HNG-03560";
+$EMAIL = "emmynuel02@gmail.com";
+$LANGUAGE = "php";
+
+echo ("Hello World, this is ".$NAME." with HNGi7 ID ".$ID." and email ".$EMAIL." using ".$LANGUAGE." for stage 2 task");
+
+?>
